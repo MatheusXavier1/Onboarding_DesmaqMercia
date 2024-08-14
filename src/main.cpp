91ed102926d9660a9a8df4c85314dff145b7b960
MatheusXavier1 <matheusxavier.20241@poli.ufrj.br>
2024-08-14 11:55:15 -0300

    Novas Alterações no teclado
    Agora o teclado só recebe 3 digitos por vez, tem um botão de enter e delete

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,11 @@
 //#include <Keypad.h>
 //#include <Arduino.h>
-//Falta comentar
-//Falta adicionar as bibliotecas
+
 char caractere;
-char tot[3] = {};
 char total[3] = {};
 char teclas[4][4] = {
 
-    {'1','2','3','A'},  
+   	{'1','2','3','A'},  
 		{'4','5','6','B'}, 
 		{'7','8','9','C'}, 
 		{'*','0','#','D'} 
@@ -31,17 +29,28 @@ void loop() {
   
   caractere = teclado.getKey();
     
-    if(caractere){
-
+  if(caractere) {
+    if(caractere != 'B') {
+    if(caractere != 'A') {
       Serial.print("Caractere: ");
       Serial.println(caractere);
 
       total[n] = caractere;
-      n++;
-      Serial.print("total: ");
-      Serial.println(total)
-      if(n==3){
-        Serial.print("FIM");
-      }
+      n++; }
+    else {
+      total[n-1] = ' ';
+      n--; }
+
+    Serial.print("total: ");
+    Serial.println(total); }
+  else {
+    Serial.print("FIM");
+
+    n = 0;
+    
+    total[0] = ' ';
+    total[1] = ' ';
+    total[2] = ' '; 
+    }
   }
 }
